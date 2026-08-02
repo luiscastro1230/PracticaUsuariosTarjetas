0b7a35a4d062d56ea6200609dccdd0c27b772aba
luiscastro1230 <sb8bk9kws5@privaterelay.appleid.com>
2026-08-01 20:05:04 -0600

    Update Tarjeta.java

--- modelo/Tarjeta.java
+++ modelo/Tarjeta.java
@@ -86,9 +86,5 @@ public class Tarjeta {
         this.idUsuario = idUsuario;
     }
 
-    public String toString() {
-        return "id: " + id + " numero: " + numero + " tipo: " + tipo
-                + " exp: " + fechaExp + " saldo: $" + saldo
-                + " activo: " + activo + " idUsuario: " + idUsuario;
-    }
+    
 }
